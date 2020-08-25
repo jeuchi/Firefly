06fa7fe7d59c6b59b780061245add82c602428be
Jeremy Euchi <jeuchi@ucsc.edu>
2020-08-24 19:07:52 -0700

    First

---
+++ FireflyDemo/View Controllers/ViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  FireflyDemo
+//
+//  Created by Jeremy  on 8/23/20.
+//  Copyright © 2020 Jeremy . All rights reserved.
+//
+
+import UIKit
+import AVKit
+
+class ViewController: UIViewController {
+    
+    var videoPlayer:AVPlayer?
+    
+    var videoPlayerLayer:AVPlayerLayer?
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        setUpElements()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        // Set up video in the background
+        setUpVideo()
+    }
+    
+    func setUpElements() {
+        
+        Utilities.styleFilledButton(signUpButton)
+        Utilities.styleHollowButton(loginButton)
+    }
+    
+    func setUpVideo() {
+        
+        // Get the path to the resource in the bundle
+        let bundlePath = Bundle.main.path(forResource: "LoginVideo", ofType: "mp4")
+        
+        guard bundlePath != nil else {
+            return
+        }
+        
+        // Create a url from it
+        let url = URL(fileURLWithPath: bundlePath!)
+        
+        // Create the video player item
+        let item = AVPlayerItem(url: url)
+        
+        // Create the player
+        videoPlayer = AVPlayer(playerItem: item)
+        
+        // Create the layer
+        videoPlayerLayer = AVPlayerLayer(player: videoPlayer!)
+        
+        // Adjust the size and frame
+        //videoPlayerLayer?.frame = CGRect(x: -self.view.frame.size.width*1.5, y: 0, width: self.view.frame.size.width*4, height: self.view.frame.size.height)
+        
+        videoPlayerLayer?.frame = CGRect(x: -self.view.frame.size.width*0.80, y: 0, width: self.view.frame.size.width*4, height: self.view.frame.size.height)
+        
+        view.layer.insertSublayer(videoPlayerLayer!, at: 0)
+        
+        // Add it to the view and play it
+        videoPlayer?.playImmediately(atRate: 0.8)
+        loopVideo(videoPlayer: videoPlayer!)
+    }
+    
+    func loopVideo(videoPlayer: AVPlayer) {
+        NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime, object: videoPlayer.currentItem, queue: .main) { [weak self] _ in
+        videoPlayer.seek(to: CMTime.zero)
+        videoPlayer.play()
+        }
+    }
+
+}
+
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'FireflyDemo' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for FireflyDemo
+	pod 'Firebase/Auth'
+	pod 'Firebase/Analytics'
+	pod 'FirebaseUI'
+	pod 'FirebaseUI/Auth'
+	pod 'Firebase/Core'
+	pod 'Firebase/Firestore'
+end
