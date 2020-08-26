2bf00a081768455ad77317a6c72de85bad32ad12
Jeremy Euchi <jeuchi@ucsc.edu>
2020-08-26 16:25:46 -0700

    tool tip video

--- FireflyDemo/View Controllers/VideoPlayback.swift
+++ FireflyDemo/View Controllers/VideoPlayback.swift
@@ -10,9 +10,12 @@ class VideoPlayback: UIViewController {
     //connect this to your uiview in storyboard
     
     @IBOutlet weak var videoView: UIView!
+    @IBOutlet weak var toolTip: UILabel!
+    @IBOutlet weak var gestureImages: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.toolTip.center = CGPoint(x: 50, y: 10)
 
         avPlayerLayer = AVPlayerLayer(player: avPlayer)
         avPlayerLayer.frame = view.bounds
@@ -25,7 +28,18 @@ class VideoPlayback: UIViewController {
         avPlayer.replaceCurrentItem(with: playerItem)
     
         avPlayer.play()
+      
+        
         loopVideo(videoPlayer: avPlayer)
+    }
+    @IBAction func showToolTip(_ sender: Any) {
+        toolTip.alpha = 1
+
+        UIView.animate(withDuration: 1) {
+            self.gestureImages.alpha = 1
+            self.gestureImages.transform = CGAffineTransform(rotationAngle: -1.6)
+            self.toolTip.center = CGPoint(x: 90, y: 90)
+        }
     }
     
     func loopVideo(videoPlayer: AVPlayer) {
