eb334c59a21cffb1f497c5bc555eba03a59e13b2
Jeremy Euchi <jeuchi@ucsc.edu>
2020-08-30 20:18:54 -0700

    feed version 3

---
+++ Firefly/View Controllers/HomeFeedViewController.swift
@@ -0,0 +1,84 @@
+//
+//  HomeFeedViewController.swift
+//  Firefly
+//
+//  Created by Jeremy  on 8/30/20.
+//  Copyright © 2020 Jeremy . All rights reserved.
+//
+
+import UIKit
+
+struct VideoModel {
+    let caption: String
+    let username: String
+    let audioTrackName: String
+    let videoFileName: String
+    let videoFileFormat: String
+}
+
+class HomeFeedViewController: UIViewController {
+    
+    private var collectionView: UICollectionView?
+
+    private var data = [VideoModel]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for _ in 0..<10 {
+            let model = VideoModel(caption: "This is a nice group", username: "@jeuchi", audioTrackName: "Ios song", videoFileName: "LoginVideo", videoFileFormat: "mp4")
+            data.append(model)
+        }
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.itemSize = CGSize(width: view.frame.size.width, height: view.frame.size.height)
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView?.register(VideoCollectionViewCell.self, forCellWithReuseIdentifier: VideoCollectionViewCell.identifier)
+        
+        collectionView?.isPagingEnabled = true
+        collectionView?.dataSource = self
+        view.addSubview(collectionView!)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView?.frame = view.bounds
+    }
+
+}
+
+extension HomeFeedViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let model = data[indexPath.row]
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: VideoCollectionViewCell.identifier, for: indexPath) as! VideoCollectionViewCell
+        
+        cell.configure(with: model)
+        cell.delegate = self
+        return cell
+    }
+}
+
+extension HomeFeedViewController: VideoCollectionViewCellDelegate {
+    func didTapLikeButton(with model: VideoModel) {
+        print("like button tapped")
+    }
+    
+    func didTapProfileButton(with model: VideoModel) {
+        print("profile button tapped")
+    }
+    
+    func didTapShareButton(with model: VideoModel) {
+        print("share button tapped")
+    }
+    
+    func didTapCommentButton(with model: VideoModel) {
+        print("comment button tapped")
+    }
+}
---
+++ Firefly/View Controllers/VideoCollectionViewCell.swift
@@ -0,0 +1,196 @@
+//
+//  VideoCollectionViewCell.swift
+//  Firefly
+//
+//  Created by Jeremy  on 8/30/20.
+//  Copyright © 2020 Jeremy . All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+protocol VideoCollectionViewCellDelegate: AnyObject {
+    func didTapLikeButton(with model: VideoModel)
+    
+    func didTapProfileButton(with model: VideoModel)
+    
+    func didTapShareButton(with model: VideoModel)
+    
+    func didTapCommentButton(with model: VideoModel)
+}
+
+class VideoCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "VideoCollectionViewCell"
+    
+    // Labels
+    
+    private let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.textColor = .white
+        return label
+    }()
+    
+    private let captionLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.textColor = .white
+        return label
+    }()
+    
+    private let audioLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.textColor = .white
+        return label
+    }()
+    
+    // Buttons
+    
+    private let profileButton: UIButton = {
+        let button = UIButton()
+        button.setBackgroundImage(UIImage(systemName: "person.circle"), for: .normal)
+        return button
+    }()
+    
+    private let likeButton: UIButton = {
+        let button = UIButton()
+        button.setBackgroundImage(UIImage(systemName: "heart.fill"), for: .normal)
+        return button
+    }()
+    
+    private let commentButton: UIButton = {
+        let button = UIButton()
+        button.setBackgroundImage(UIImage(systemName: "text.bubble.fill"), for: .normal)
+        return button
+    }()
+    
+    private let shareButton: UIButton = {
+        let button = UIButton()
+        button.setBackgroundImage(UIImage(systemName: "arrorshape.turn.up.right.fill"), for: .normal)
+        return button
+    }()
+    
+    private let videoContainer = UIView()
+    
+    // Delegate
+    
+    weak var delegate: VideoCollectionViewCellDelegate?
+    
+    // Subviews
+    var player: AVPlayer?
+    
+    private var model: VideoModel?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.backgroundColor = .black
+        contentView.clipsToBounds = true
+        addSubviews()
+    }
+    
+    private func addSubviews() {
+        
+        contentView.addSubview(videoContainer)
+        
+        contentView.addSubview(usernameLabel)
+        contentView.addSubview(captionLabel)
+        contentView.addSubview(audioLabel)
+
+        contentView.addSubview(profileButton)
+        contentView.addSubview(likeButton)
+        contentView.addSubview(commentButton)
+        contentView.addSubview(shareButton)
+        
+        // Add actions
+        likeButton.addTarget(self, action: #selector(didTapLikeButton), for: .touchDown)
+        profileButton.addTarget(self, action: #selector(didTapProfileButton), for: .touchDown)
+        commentButton.addTarget(self, action: #selector(didTapCommentButton), for: .touchDown)
+        shareButton.addTarget(self, action: #selector(didTapShareButton), for: .touchDown)
+    
+        videoContainer.clipsToBounds = true
+        contentView.sendSubviewToBack(videoContainer)
+    }
+    
+    @objc private func didTapLikeButton() {
+        guard let model = model else { return }
+        delegate?.didTapLikeButton(with: model)
+    }
+    
+    @objc private func didTapCommentButton() {
+        guard let model = model else { return }
+        delegate?.didTapCommentButton(with: model)
+    }
+    
+    @objc private func didTapShareButton() {
+        guard let model = model else { return }
+        delegate?.didTapShareButton(with: model)
+    }
+    
+    @objc private func didTapProfileButton() {
+        guard let model = model else { return }
+        delegate?.didTapProfileButton(with: model)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        videoContainer.frame = contentView.bounds
+        
+        let size = contentView.frame.size.width/7
+        let width = contentView.frame.size.width
+        let height = contentView.frame.size.height - 100
+        
+        // Buttons
+        shareButton.frame = CGRect(x: width-size, y: height-size, width: size, height: size)
+        commentButton.frame = CGRect(x: width-size, y: height-(size*2)-10, width: size, height: size)
+        likeButton.frame = CGRect(x: width-size, y: height-(size*3)-10, width: size, height: size)
+        profileButton.frame = CGRect(x: width-size, y: height-(size*4)-10, width: size, height: size)
+    
+        // Labels
+        // username, caption, audio
+        audioLabel.frame = CGRect(x: 5, y: height-30, width: width-size-10, height: 50)
+        captionLabel.frame = CGRect(x: 5, y: height-80, width: width-size-10, height: 50)
+        usernameLabel.frame = CGRect(x: 5, y: height-120, width: width-size-10, height: 50)
+    }
+    
+   /* override func prepareForReuse() {
+        super.prepareForReuse()
+        captionLabel.text = nil
+        audioLabel.text = nil
+        usernameLabel.text = nil
+    }*/
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public func configure(with model: VideoModel) {
+        self.model = model
+        configureVideo()
+        
+        // labels
+        captionLabel.text = model.caption
+        audioLabel.text = model.audioTrackName
+        usernameLabel.text = model.username
+    }
+    
+    private func configureVideo() {
+        guard let model = model else {
+            return
+        }
+        guard let path = Bundle.main.path(forResource: model.videoFileName, ofType: model.videoFileFormat) else {
+            return
+        }
+        player = AVPlayer(url: URL(fileURLWithPath: path))
+        
+        let playerView = AVPlayerLayer()
+        playerView.player = player
+        playerView.frame = contentView.bounds
+        playerView.videoGravity = .resizeAspectFill
+        videoContainer.layer.addSublayer(playerView)
+        player?.play()
+    }
+}
+
